56b887166442ddbdf707a5717368c44f6e74f6bb
NompiloZondo-Dev <nompilo.n.zondo@gmail.com>
2024-11-12 14:02:37 +0200

    tick

--- app.js
+++ app.js
@@ -25,21 +25,21 @@ app.get('/api/tasks', (req, res) => {
 
 // Route to add a new To-Do item
 app.post('/add', (req, res) => {
-<<<<<<< HEAD
+
   const todoText = req.body.todoText;
   if (todoText) {
     todos.push({ text: todoText, completed: false }); // Ensure the 'text' property is added
   }
   res.redirect('/'); // Redirect to the homepage
 });
-=======
-    const todoText = req.body.todoText;
-    if (todoText) {
-      todos.push({ text: todoText, completed: false });
-    }
-    res.redirect('/');
-  });
->>>>>>> c947bdaf34e568d0663375ec0cfd90a2b5e4bfbe
+
+  //   const todoText = req.body.todoText;
+  //   if (todoText) {
+  //     todos.push({ text: todoText, completed: false });
+  //   }
+  //   res.redirect('/');
+  // });
+
 
 // Route to mark a To-Do as completed
 app.post('/complete', (req, res) => {
